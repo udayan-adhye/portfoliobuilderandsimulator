0004a51b76f4da0cb44fba2f409c739a2865bf13
Udayan Adhye <u.adhye95@gmail.com>
2026-03-25 11:26:56 +0530

    Remove dist from repo, let Vercel build fresh

--- dist/assets/worker-c4c5cfc1.js
+++
@@ -1,2 +0,0 @@
-self.onmessage=async function(s){const{navDataList:t,years:a,allocations:e,investmentAmount:n}=s.data,o=(await import("./index-6083953c.js")).calculateLumpSumRollingXirr(t,a,e,n);self.postMessage(o)};
-//# sourceMappingURL=worker-c4c5cfc1.js.map
--- dist/assets/worker-fae8a902.js
+++
@@ -1,2 +0,0 @@
-self.onmessage=async function(a){const{navDataList:e,years:n,allocations:s,rebalancingEnabled:l,rebalancingThreshold:t,includeNilTransactions:o,stepUpEnabled:i,stepUpPercentage:c,sipAmount:r}=a.data,p=(await import("./index-225ae7d1.js")).calculateSipRollingXirr(e,n,s,l,t,o,i,c,r);self.postMessage(p)};
-//# sourceMappingURL=worker-fae8a902.js.map
